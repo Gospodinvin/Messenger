ea5ada1fd026ca305d5fab133ec6e7bb420b935d
Gospodinvin <158985517+Gospodinvin@users.noreply.github.com>
2026-06-16 09:29:26 +0500

    Refactor App.js to import from src/App

--- App.js
+++ App.js
@@ -1,10 +1,2 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>TeamMate Pro работает!</Text>
-    </View>
-  );
-}
+import App from './src/App';
+export default App;
